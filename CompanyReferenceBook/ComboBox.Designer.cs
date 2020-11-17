f3d1b455ba7ef925fef1081fa81008341dc21c08
Ilia Fedorov <fedoroff.ilia@gmail.com>
2020-11-17 23:59:59 +0500

    Revert "Revert "40:39""
    This reverts commit deaad38f9321d44f3f5bd1a9be537d854a206760.
    
    # Conflicts:
    #	CompanyReferenceBook/CompanyReferenceBook.csproj
    #	CompanyReferenceBook/FormReview2.Designer.cs
    #	CompanyReferenceBook/FormReview2.cs

---
+++ CompanyReferenceBook/ComboBox.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace CompanyReferenceBook
+{
+    partial class ComboBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbCourse = new System.Windows.Forms.ComboBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Course :";
+            // 
+            // cmbCourse
+            // 
+            this.cmbCourse.FormattingEnabled = true;
+            this.cmbCourse.Location = new System.Drawing.Point(87, 31);
+            this.cmbCourse.Name = "cmbCourse";
+            this.cmbCourse.Size = new System.Drawing.Size(191, 21);
+            this.cmbCourse.TabIndex = 1;
+            this.cmbCourse.SelectedIndexChanged += new System.EventHandler(this.cmbCourse_SelectedIndexChanged);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(87, 67);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(79, 26);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // ComboBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(323, 290);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.cmbCourse);
+            this.Controls.Add(this.label1);
+            this.Name = "ComboBox";
+            this.Text = "ComboBox";
+            this.Load += new System.EventHandler(this.ComboBox_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbCourse;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
