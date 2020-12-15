d274aa275a706de0c134b70fe584fc5ae9965ac9
Ilia Fedorov <fedoroff.ilia@gmail.com>
2020-12-15 10:11:44 +0300

    перенес строку подключения к БД в отдельный класс синглтон
    и сделал соответстующие замены в Login и Registration формах

---
+++ CompanyReferenceBook/Helper/DbConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class DbConnection
+{
+    private static readonly DbConnection instance = new DbConnection();
+
+    public string path { get; private set; }
+
+    private DbConnection()
+    {
+        path = @"Data Source=DESKTOP-M3TGEAF;Initial Catalog=registration;Integrated Security=True";
+    }
+
+    public static DbConnection GetInstance()
+    {
+        return instance;
+    }
+}
